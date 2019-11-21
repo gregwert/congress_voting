dcd19bc208929c360444406af2489d57387afbfb
gregwert <gaw8pa@virginia.edu>
2019-11-20 20:55:26 -0500

    added year converter and changed init

--- processing/utils/__init__.py
+++ processing/utils/__init__.py
@@ -1,2 +1,2 @@
 from .scraping_utils import folder_maker, issue_update
-from.transforms import yea_marker, year_to_session
+from.transforms import *
--- processing/utils/transforms.py
+++ processing/utils/transforms.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def yea_marker(x):
     """  Function to convert yes votes to 1 """
     return 1 if x == 'Present' or x == 'Yea' else 0
@@ -6,3 +9,8 @@ def yea_marker(x):
 def year_to_session(year: int) -> int:
     """ Maps the year to the session of congress """
     return int((year - 1788 + year % 2)/2)
+
+
+def session_to_years(session: int) -> Tuple[int, int]:
+    year = int(2*session + 1788)
+    return year-1, year
