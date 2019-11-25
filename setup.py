115282988cb1ec644d80e5e5404b9f4590a8996f
gregwert <gaw8pa@virginia.edu>
2019-11-25 01:07:36 -0500

    updated setup file to properly build

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read().replace('# ', '').replace('\n', ' | ')
+
 setup(
     name='congress_voting',
     description='Scrape and Examine Congressional Voting Data',
     author_email='gregawert@gmail.com',
     url={'github': 'https://github.com/gregwert/congress_voting'},
-    version='0.1dev',
+    version='0.0.1dev',
     python_requires='>=3.6',
     package_dir={'': 'congress_voting'},
-    packages=find_packages(where='congress_voting'),
-    install_requires=['pandas'],
-    long_description=open('README.md').read(),
+    packages=find_packages('congress_voting'),
+    install_requires=[
+        'pandas>=0.25.3'
+    ],
+    long_description=long_description,
 )
