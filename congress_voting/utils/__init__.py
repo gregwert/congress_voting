8cced29d2b92a9554a2bbdfbf4bab68000ac29d4
gregwert <gaw8pa@virginia.edu>
2019-11-29 02:32:52 -0500

    updated imports to reference package

--- congress_voting/utils/__init__.py
+++ congress_voting/utils/__init__.py
@@ -1,2 +1,2 @@
-from .scraping_utils import folder_maker, issue_update
-from.transforms import *
+from congress_voting.utils.scraping_utils import folder_maker, issue_update
+from congress_voting.utils.transforms import *
